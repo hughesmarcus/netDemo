ad92dc02ad0c78b2cfdfd02b5146641157e9431b
Marcus Hughes <marcushughes80@gmail.com>
2017-04-13 11:57:04 -0400

    Update BuyerController.cs

--- WebApplication1/Controllers/BuyerController.cs
+++ WebApplication1/Controllers/BuyerController.cs
@@ -42,8 +42,6 @@ namespace WebApplication1.Controllers
         }
 
         // POST: Buyer/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,Phone,Name,Address,Email")] Buyer buyer)
